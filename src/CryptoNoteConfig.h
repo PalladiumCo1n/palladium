1640336cda2f58b9136995d44fb92ca99f23b2c9
Lukas Olson <lukasdragon@live.ca>
2017-05-14 14:00:05 -0700

    Updated Genesis Hash

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "palladiumcoin";
-const char GENESIS_COINBASE_TX_HEX[] = "013c01ff00019da417029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101fb331ee57585fc3394de1cde332d24a30eb164f32981983c03364ec135909518";
+const char GENESIS_COINBASE_TX_HEX[] = "013c01ff000129029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210136f48671a35a7aa0c372567177f25b3ff4680b40ab6c4f703427ebe8fc68c2e1";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
